6536e72ffa5c7fe3b85356c99546be0d532aaa59
Mana <manakouwenberg@gmail.com>
2021-10-01 12:48:30 +0200

    Created TamaMossy App

---
+++ TamaMossy/TamaMossy/TamaMossy/TamaMossy/AppShell.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using TamaMossy.Views;
+using Xamarin.Forms;
+
+namespace TamaMossy
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
---
+++ TamaMossy/TamaMossy/TamaMossy/TamaMossy/Views/StatPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+using TamaMossy.Models;
+using Xamarin.Forms.Xaml;
+
+namespace TamaMossy.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StatPage : ContentPage
+    {
+        string filename = "current_state.txt";
+        public CurrentState curState;
+        public StatPage()
+        {
+            InitializeComponent();            
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (File.Exists(Path.Combine(App.FolderPath, filename)))
+            {
+                curState = CurrentState.ParseFromString(File.ReadAllText(Path.Combine(App.FolderPath, filename)));
+                //Load stats here
+            }
+            else
+            {
+                curState = new CurrentState();
+                File.WriteAllText(Path.Combine(App.FolderPath, filename), curState.ParseToString());
+            }
+        }
+    }
+}
