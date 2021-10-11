154aa46f577b920f85e9ed1090868ea5115ed6be
Mana <manakouwenberg@gmail.com>
2021-10-11 14:09:42 +0200

    Datastore interface added

---
+++ TamaMossy/TamaMossy/TamaMossy/Models/IDataStore.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TamaMossy.Models
+{
+    interface IDataStore<T>
+    {
+        bool CreateItem(T item);
+
+        T ReadItem();
+
+        bool UpdateItem(T item);
+
+        bool DeleteItem(T item);
+    }
+}
